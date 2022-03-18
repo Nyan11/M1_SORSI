f05ae50f65a8680a9879e91a7594fdc98ebcaec7
Nyan11 <y.legoff97@gmail.com>
2022-03-18 08:55:37 +0100

    [back] - MaJ delCours

--- back/src/routes/delete/delCours.js
+++ back/src/routes/delete/delCours.js
@@ -4,40 +4,50 @@ const router = require('express').Router();
 router.delete('/cours', (req, res) => {
 
     let id_cours = req.body.idCours;
-    console.log(req.body)
     if (!id_cours) {
-        res.status(400).json({
+        return res.status(400).json({
             error: true,
             message:  "Merci de spécifier l'id du cours à supprimer"
         });
-        return;
     }
 
-    const SQLRequest = "DELETE FROM COURS WHERE idCours = ?";
-    connexion.query(SQLRequest,[id_cours] , (error , result) => {
+    //Vérification que le cours ne sois pas lier à des créneaux
+    const SQLRequest = "SELECT * FROM COURS INNER JOIN CRENEAU ON COURS.idCours = CRENEAU.idCours WHERE COURS.idCours = ?";
+    connexion.query(SQLRequest,[id_cours],async (error, result) => {
 
         if(error) {
-          res.status(500).json({
-              error: true,
-              message: error,
-          });
+          res.status(500).json({err: error});
         }
-
-        if (result.affectedRows === 0) {
+        if (result.length > 0) {
             res.status(200).json({
-                error: false,
-                message: "Cours supprimé avec succès"
+                "error":true,
+                "message":"Impossible de supprimer le cours car il est lié à des créneaux"
             });
-            return;
         }
         else {
-            res.status(404).json({
-                error: true,
-                message: "Aucun cours avec cette id trouvé !"
+
+            //Suppression du cours
+            const SQLRequest = "DELETE FROM COURS WHERE idCours = ?";
+            await connexion.query(SQLRequest, [id_cours], async (error, result) => {
+
+              if(error) {
+                res.status(500).json({err: error});
+              }
+
+                if (result.affectedRows > 0) {
+                    res.status(200).json({
+                        error: false,
+                        message: "Le cours à bien été supprimé !"
+                    });
+                    console.log("FUNCTION CALL : [DELETE] - Suppression du cours avec l'id " + id_cours);
+                } else {
+                    res.status(404).json({
+                        error: true,
+                        message: "Aucun cours avec cet id trouvé !"
+                    });
+                }
             });
-            return;
         }
-        console.log("FUNCTION CALL : [DELETE] - Suppression d'un cours");
     });
 });
 
